a36c28dd430d061b1106b930469cd7f500a6d161
William Griffiths <willjgriff@gmail.com>
2019-06-16 13:16:21 +0100

    Continued working on the permission with params script.

--- livepeer-aragon-app/scripts/grantPermissionWithParameters.js
+++ livepeer-aragon-app/scripts/grantPermissionWithParameters.js
@@ -4,9 +4,9 @@ const Agent = artifacts.require("Agent.sol")
 
 const ACL_ADDRESS = "0x184c81675e89b6445befad31f9dfbd63b8c95a36"
 const AGENT_ADDRESS = "0xfBCBa648Dff9dB0d8aA472bA9CD3e24fEe7B041D"
-
 const LIVEPEER_APP_ADDRESS = "0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb"
-const ANY_ADDRESS = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF"
+
+const ANY_ACCOUNT = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF"
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 const FUNCTION_SIGNATURES = {
@@ -26,30 +26,42 @@ const createParam = (argId, operation, functionSignature) => {
     return new bn(`${argId}${operation}${value}`, 16)
 }
 
+const grantPermissionToExecuteFunction = async (permissionManager, acl, agent, functionSignature) => {
+
+    console.log(`Granting permission to execute ${functionSignature} to ${agent.address}...`)
+
+    const param = createParam("0x02", "01", functionSignature)
+    const role = await agent.EXECUTE_ROLE()
+
+    const rolePermissionManager = await acl.getPermissionManager(agent.address, role)
+
+    if (rolePermissionManager === ZERO_ADDRESS) {
+        const createPermissionReceipt = await acl.createPermission(ZERO_ADDRESS, agent.address, role, permissionManager)
+        console.log(`Permission created: ${createPermissionReceipt.tx}`)
+    }
+
+    const grantPermissionReceipt = await acl.grantPermissionP(LIVEPEER_APP_ADDRESS, agent.address, role, [param], { from: permissionManager })
+    console.log(`Permission granted: ${grantPermissionReceipt.tx}`)
+
+    const permissionGrantedToZeroAddress = await acl.hasPermission(ZERO_ADDRESS, agent.address, role)
+    if (permissionGrantedToZeroAddress) {
+        const revokePermissionReceipt = await acl.revokePermission(ZERO_ADDRESS, agent.address, role)
+        console.log(`Permission revoked for ${ZERO_ADDRESS}: ${revokePermissionReceipt.tx}`)
+    }
+}
+
 module.exports = async () => {
 
     try {
-        const functionSignature = FUNCTION_SIGNATURES.BOND
-
-        console.log(`Granting permission to execute ${functionSignature} to ${AGENT_ADDRESS}...`)
-
         const permissionManager = (await web3.eth.getAccounts())[0]
         const acl = await ACL.at(ACL_ADDRESS)
         const agent = await Agent.at(AGENT_ADDRESS)
+        const functionSignature = FUNCTION_SIGNATURES.APPROVE
 
-        const param = createParam("0x02", "01", functionSignature)
-        const role = await agent.EXECUTE_ROLE()
+        await grantPermissionToExecuteFunction(permissionManager, acl, agent, functionSignature)
 
-        const rolePermissionManager = await acl.getPermissionManager(agent.address, role)
-
-        if (rolePermissionManager === ZERO_ADDRESS) {
-            const createPermissionReceipt = await acl.createPermission(ZERO_ADDRESS, agent.address, role, permissionManager)
-        }
-
-        const grantPermissionReceipt = await acl.grantPermissionP(LIVEPEER_APP_ADDRESS, AGENT_ADDRESS, role, [param], { from: permissionManager })
+        // TODO: Add and test revokePermission function.
         // const grantPermissionReceipt = await acl.revokePermission(GRANT_TO_ADDRESS, LIVEPEER_APP_ADDRESS, role)
-
-        console.log(`Permission granted: ${grantPermissionReceipt.tx}`)
 
         process.exit()
 
